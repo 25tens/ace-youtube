bc78da9aec26424911b525b567352505f14caf0c
harnessjr <25tens@gmail.com>
2018-04-04 15:26:16 +0900

    modified ace/settings.py

--- ace/settings.py
+++ ace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rrov-6()=3ow($h+53-g+$ts(u3p@z*ye^-29=)2+ix+93ymu$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
